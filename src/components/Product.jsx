f600c46785da67b311238783ed76cbd746e0de8f
Svitlana Kyryk <kyryk.svitlana@gmail.com>
2024-07-16 23:29:36 +0200

    ап

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,41 @@
 // src/Product.jsx
 
-const Product = () => {
-      return (
-        <div>
-          <h2>Tacos</h2>
-                <p>Price: 999 credits</p>
-        </div>
-      );
-    };
+// const Product = () => {
+//       return (
+//         <div>
+//           <h2>Tacos</h2>
+//                 <p>Price: 999 credits</p>
+//         </div>
+//       );
+//     };
     
-    export default Product;
-    
+//     export default Product;
+   
+// src/components/Product.jsx
+
+// export const Product = () => {
+// 	const price = 999;
+// 	const imgUrl = "<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>";
+
+//   return (
+//     <>
+//       <h2>Tacos</h2>
+// 			<img src={imgUrl} alt="Tacos With Lime" width="640" />
+//       <p>Price: {price} credits</p>
+//     </>
+//   );
+// };
+
+//     export default Product;
+
+// src/components/Product.jsx
+
+export const Product = props => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.imgUrl} alt={props.name} width="480" />
+      <p>Price: {props.price} credits</p>
+    </div>
+  );
+};
