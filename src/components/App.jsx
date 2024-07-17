1a1124b0412e60a3aa64734566badf8b2c3f2d4f
Svitlana Kyryk <kyryk.svitlana@gmail.com>
2024-07-17 18:19:19 +0200

    ап

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -91,21 +91,80 @@
 
 import { Product } from "./Product";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Best selling</h1>
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Best selling</h1>
 
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
-    </div>
+//       <Product
+//         name="Tacos With Lime"
+//         imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+//         price={10.99}
+//       />
+//       <Product
+//         name="Fries and Burger"
+//         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+//         price={14.29}
+//       />
+//     </div>
+//   );
+// }
+// src/components/App.jsx
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Best selling</h1>
+
+//       <Product
+//         name="Tacos With Lime"
+//         price={10.99}
+//       />
+//       <Product
+//         name="Fries and Burger"
+//         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+//         price={14.29}
+//       />
+//     </div>
+//   );
+// }
+
+const favouriteBooks = [
+  { id: "id-1", name: "JS for beginners" },
+  { id: "id-2", name: "React basics" },
+  { id: "id-3", name: "React Router overview" }
+];
+
+const BookList = ({ books }) => {
+  return (
+		<ul>
+			{books.map((book) => {
+			  return <li key={book.id}>{book.name}</li>;
+			})}
+		</ul>
   );
-}
+};
+const Card = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+const App = () => {
+  return (
+    <>
+			<h1>Books of the week</h1>
+      <BookList books={favouriteBooks} />
+      <Card>
+	<h1>Card title</h1>
+	<p>Text between opening and closing tag</p>
+</Card>
+    </>
+  );
+};
+
+export default App;
+
+// Оголошення
+
+
+// Використання
+
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -30,12 +30,40 @@
 
 // src/components/Product.jsx
 
-export const Product = props => {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <img src={props.imgUrl} alt={props.name} width="480" />
-      <p>Price: {props.price} credits</p>
-    </div>
-  );
+// export const Product = props => {
+//   return (
+//     <div>
+//       <h2>{props.name}</h2>
+//       <img src={props.imgUrl} alt={props.name} width="480" />
+//       <p>Price: {props.price} credits</p>
+//     </div>
+//   );
+// };
+
+// export const Product = ({ name, imgUrl, price }) => {
+//   return (
+//     <div>
+//       <h2>{name}</h2>
+// 			<img src={imgUrl} alt={name} width="480" />
+// 			<p>Price: {price} credits</p>
+//     </div>
+//   );
+// };
+export const Product = ({
+  name,
+  imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+  price,
+}) => (
+  <div>
+		<h2>{name}</h2>
+    <img src={imgUrl} alt={name} width="480" />
+    <p>Price: {price} credits</p>
+  </div>
+)
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  imgUrl: PropTypes.string,
+  price: PropTypes.number.isRequired,
 };
+
+export default Product;
