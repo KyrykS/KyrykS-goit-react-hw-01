b7b520dddffa00fee4d11cab56b2335055fb8a9f
Svitlana Kyryk <kyryk.svitlana@gmail.com>
2024-07-22 21:04:44 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import Profile from './Profile';
-import userData from '../userData.json'
+import Profile from './Profile/Profile';
+import FriendList from './FriendList/FriendList';
+import userData from '../userData.json';
+import friends from '../friends.json';
+
 const App = () => {
   return (
     <div>
@@ -11,8 +14,10 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
     </div>
   );
 };
 
 export default App;
+
